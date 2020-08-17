521f1afd88fb91afb075e743492df00b40d844d4
iZuno-K <izumikarino@gmail.com>
2020-08-17 20:58:46 +0900

    [mod] modify logging of uwsgi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RecomendLunch',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='iZuno-K',
+    author_email='',
+    description=''
+)
