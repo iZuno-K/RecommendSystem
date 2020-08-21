9d408394f073aef4f80a5f7f5518def7d5944f39
iZuno-K <izumikarino@gmail.com>
2020-08-21 22:57:22 +0900

    [mod] add rule base response.

--- make_response.py
+++ make_response.py
@@ -1,7 +1,115 @@
+import random
+import re
+LUNCH_LIST = {}
+count = 0
+# TODO 複数人が接続しても大丈夫なようにする。
+
 def make_response_two_ai(json_dict):
-    responses = [json_dict["#chat_{}".format(len(json_dict) - 1)] + "はちょっと...",
-                 json_dict["#chat_{}".format(len(json_dict) - 1)] + "はお勧め",
-                 "まあそう言わないで",
-                 "いやいういう"]
-    _classes = ['talk_left1', 'talk_left2', 'talk_left1', 'talk_left2']
+    global count, LUNCH_LIST
+    count += 1
+
+    l = len(list(json_dict.keys()))
+    responses = []
+    _classes = []
+    if l == 1:
+        # global 変数初期化
+        LUNCH_LIST = {"和食": ["唐揚げ", "おにぎり", "カレー", "焼肉", "そば"]}
+        count = 0
+
+        responses = ["和食、洋食、中華どれがいい？"]
+        _classes = ['talk_left1']
+    elif l == 3:
+        categories = ["和食", "洋食", "中華"]
+        human_choice_category = json_dict["#chat_{}".format(len(json_dict) - 1)]
+        # カテゴリ判定
+        for cat in categories:
+            if cat in human_choice_category:
+                c = cat
+                break
+        ai1_item = random.choice(LUNCH_LIST[c])
+        LUNCH_LIST[c].remove(ai1_item)
+        ai2_item = random.choice(LUNCH_LIST[c])
+        LUNCH_LIST[c].remove(ai2_item)
+
+        ai1_response1 = "{}にしたらどう？".format(ai1_item)
+        ai2_response1 = "やっぱ{}でしょ！？".format(ai2_item)
+        ai1_response2 = "また{}食べるとママに怒られるよ".format(ai2_item)
+        ai2_response2 = "きっとパパも{}食べたいはずだよ".format(ai2_item)
+
+        responses = [ai1_response1, ai2_response1, ai1_response2, ai2_response2]
+        _classes = ['talk_left1', 'talk_left2', 'talk_left1', 'talk_left2']
+    elif l == 8:
+        categories = ["和食", "洋食", "中華"]
+        human_choice_category = json_dict["#chat_{}".format(len(json_dict) - 6)]
+        latest_human_response = json_dict["#chat_{}".format(len(json_dict) - 1)]
+        # カテゴリ判定
+        for cat in categories:
+            if cat in human_choice_category:
+                c = cat
+                break
+        # TODO positve negative detection
+        positive = False
+        if positive:
+            responses, _classes = response_when_accepted(json_dict, c)
+        else:
+            # ここで両方否定された場合はai2から喋る
+            prev_ai1_item = json_dict["#chat_{}".format(len(json_dict) - 5)][:-8]
+            prev_ai2_item = json_dict["#chat_{}".format(len(json_dict) - 4)][3:-5]
+
+            ai2_item = random.choice(LUNCH_LIST[c])
+            LUNCH_LIST[c].remove(ai2_item)
+            ai1_item = random.choice(LUNCH_LIST[c])
+            LUNCH_LIST[c].remove(ai1_item)
+
+            ai2_response1 = "{}はやめておこう。{}にしよう！".format(prev_ai2_item, ai2_item)
+            ai1_response1 = "じゃあ{}はどう？".format(ai1_item)
+
+            responses = [ai2_response1, ai1_response1]
+            _classes = ['talk_left2', 'talk_left1']
+    if l == 11:
+        categories = ["和食", "洋食", "中華"]
+        human_choice_category = json_dict["#chat_{}".format(len(json_dict) - 9)]
+        latest_human_response = json_dict["#chat_{}".format(len(json_dict) - 4)]
+        # カテゴリ判定
+        for cat in categories:
+            if cat in human_choice_category:
+                c = cat
+                break
+        # TODO positve negative detection
+        positive = True
+        if positive:
+            responses, _classes = response_when_accepted(json_dict, c)
+        else:
+            # ここまでで決まらなかったらギブアップ
+            ai1_response1 = "力不足でござった..."
+            ai2_response1 = "お手上げです..."
+            responses = [ai1_response1, ai2_response1]
+            _classes = ['talk_left1', 'talk_left2']
+
     return responses, _classes
+
+
+def response_when_accepted(json_dict, category):
+    l = len(list(json_dict.keys()))
+    if l == 8:
+        prev_ai1_item = json_dict["#chat_{}".format(len(json_dict) - 5)][:-7]
+        prev_ai2_item = json_dict["#chat_{}".format(len(json_dict) - 4)][3:-5]
+    elif l == 11:
+        prev_ai1_item = json_dict["#chat_{}".format(len(json_dict) - 2)][3:-4]
+        prev_ai2_item = re.findall(r"\w+はやめておこう。(\w+)にしよう！", json_dict["#chat_{}".format(len(json_dict) - 3)])[0]
+    else:
+        return [], []
+    latest_human_response = json_dict["#chat_{}".format(len(json_dict) - 1)]
+
+    if prev_ai1_item in latest_human_response:
+        accepted_side1 = "そうだね{}にしよう！".format(prev_ai1_item)
+        _classes = ['talk_left1', 'talk_left2', 'talk_left2', 'talk_left1']
+    else:
+        accepted_side1 = "そうだね{}にしよう！".format(prev_ai2_item)
+        _classes = ['talk_left2', 'talk_left1', 'talk_left1', 'talk_left2']
+
+    denied_side1 = "えーっ！じゃ、じゃあ・・・"
+    denied_side2 = "{}なんてオススメだよ！".format(random.choice(LUNCH_LIST[category]))
+    accepted_side2 = "もうええわー！"
+    responces = [accepted_side1, denied_side1, denied_side2, accepted_side2]
+    return responces, _classes
