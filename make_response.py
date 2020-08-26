df581caef2f95e3a6cc2074bd96778ff46d3525f
iZuno-K <izumikarino@gmail.com>
2020-08-26 21:23:30 +0900

    [mod] "和洋中" 一文字返答に対応

--- make_response.py
+++ make_response.py
@@ -23,14 +23,11 @@ def make_response_two_ai(json_dict):
         responses = ["和食、洋食、中華どれがいい？".format(count)]
         _classes = ['talk_left1']
     elif l == 3:
-        categories = ["和食", "洋食", "中華"]
         human_choice_category = json_dict["#chat_{}".format(len(json_dict) - 1)]
         # カテゴリ判定
-        for cat in categories:
-            if cat in human_choice_category:
-                c = cat
-                break
-
+        c = find_category(human_choice_category)
+        if c is None:
+            return [], []
         ai1_item = random.choice(LUNCH_LIST[LUNCH_LIST["category1"] == c]["name"].tolist())
         LUNCH_LIST = LUNCH_LIST[LUNCH_LIST["name"] != ai1_item]  # remove ai1_item from subsequent candidates
         ai2_item = random.choice(LUNCH_LIST[LUNCH_LIST["category1"] == c]["name"].tolist())
@@ -44,14 +41,12 @@ def make_response_two_ai(json_dict):
         responses = [ai1_response1, ai2_response1, ai1_response2, ai2_response2]
         _classes = ['talk_left1', 'talk_left2', 'talk_left1', 'talk_left2']
     elif l == 8:
-        categories = ["和食", "洋食", "中華"]
         human_choice_category = json_dict["#chat_{}".format(len(json_dict) - 6)]
         latest_human_response = json_dict["#chat_{}".format(len(json_dict) - 1)]
         # カテゴリ判定
-        for cat in categories:
-            if cat in human_choice_category:
-                c = cat
-                break
+        c = find_category(human_choice_category)
+        if c is None:
+            return [], []
         positive = judge_np(latest_human_response)
         if positive:
             responses, _classes = response_when_accepted(json_dict, c)
@@ -71,14 +66,13 @@ def make_response_two_ai(json_dict):
             responses = [ai2_response1, ai1_response1]
             _classes = ['talk_left2', 'talk_left1']
     if l == 11:
-        categories = ["和食", "洋食", "中華"]
         human_choice_category = json_dict["#chat_{}".format(len(json_dict) - 9)]
         latest_human_response = json_dict["#chat_{}".format(len(json_dict) - 1)]
         # カテゴリ判定
-        for cat in categories:
-            if cat in human_choice_category:
-                c = cat
-                break
+        c = find_category(human_choice_category)
+        if c is None:
+            return [], []
+
         positive = judge_np(latest_human_response)
         if positive:
             responses, _classes = response_when_accepted(json_dict, c)
@@ -116,3 +110,68 @@ def response_when_accepted(json_dict, category):
     accepted_side2 = "もうええわー！"
     responces = [accepted_side1, denied_side1, denied_side2, accepted_side2]
     return responces, _classes
+
+
+def find_category(txt):
+    c = None
+    categories = ["和食", "洋食", "中華", "和", "洋", "中"]
+    for cat in categories:
+        if cat in txt:
+            c = cat
+            break
+    if c == "和":
+        c = "和食"
+    elif c == "洋":
+        c = "洋食"
+    elif c == "中":
+        c = "中華"
+    return c
+
+def recommend_item_detector(json_dict):
+    l = len(json_dict)
+    if l == 8:
+        prev_ai1_items = [re.findall(r"(\w+)にしたらどう？", json_dict["#chat_{}".format(l - 5)])[0]]
+        prev_ai2_items = [re.findall(r"やっぱ(\w+)でしょ！？", json_dict["#chat_{}".format(l - 4)])[0]]
+    elif l == 11:
+        prev_ai1_items = [re.findall(r"(\w+)にしたらどう？", json_dict["#chat_{}".format(l - 8)])[0]]
+        prev_ai2_items = [re.findall(r"やっぱ(\w+)でしょ！？", json_dict["#chat_{}".format(l - 7)])[0]]
+        prev_ai1_items.append(re.findall(r"\w+はやめておこう。(\w+)にしよう！", json_dict["#chat_{}".format(len(json_dict) - 3)])[0])
+        prev_ai2_items.append(re.findall(r"\w+はやめておこう。(\w+)にしよう！", json_dict["#chat_{}".format(len(json_dict) - 3)])[0])
+    else:
+        prev_ai1_items = []
+        prev_ai2_items = []
+
+    return prev_ai1_items, prev_ai2_items
+
+def judge(human_response, json_dict):
+    positive = False
+    judged_side = "both"
+    ai_lunch = recommend_item_detector(json_dict)
+    for i in range(1, 1 + 2):
+        for l in ai_lunch[i]:
+            if l ==  human_response:
+                # 人間のレスポンスが料理名だけだった場合、それはその料理名を肯定したことになる
+                positive = _judge(l, human_response)
+                judged_side = str(i)
+                return positive, judged_side
+            elif l in human_response:
+                positive = _judge(l, human_response)
+                judged_side = str(i)
+                return positive, judged_side
+            else:
+                positive = _judge(l, human_response)
+                judged_side = "both"
+    return positive, judged_side
+
+
+def _judge(lunch, human_response):
+    if lunch == human_response:
+        # 人間のレスポンスが料理名だけだった場合、それはその料理名を肯定したことになる
+        positive = True
+        return positive
+    elif lunch in human_response:
+        positive = judge_np(human_response)
+        return positive
+    else:
+        positive = judge_np(human_response)
+        return positive
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
 
-Mecab install
+## 設定
+### Mecab install
 ```
 sudo apt install mecab
 sudo apt install libmecab-dev
 sudo apt install mecab-ipadic-utf8
 sudo apt install swig
-pip install mecab-python3
+```
+### python3
+```
+pip install -r requirements.txt
 ```
